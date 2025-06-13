279ac557fdc86c2ef98c85f946e4f3d31220df92
Alaa-rh.name <alaarahmoun44@gmail.com>
2025-06-13 06:50:49 -0400

    second commit

--- DataAccess/TaskRepository.cs
+++ DataAccess/TaskRepository.cs
@@ -28,11 +28,13 @@ namespace NTierTodoApp.DataAccess
         }
 
         // TODO: تنفيذ دالة حذف المهمة
-        public void Delete(int id)
+       public void Delete(int id)
         {
-            // TODO: ابحث عن المهمة باستخدام id
-
-            // TODO: إذا كانت المهمة موجودة، قم بإزالتها من القائمة
+            var task = tasks.FirstOrDefault(t => t.Id == id);
+            if (task != null)
+            {
+                tasks.Remove(task);
+            }
         }
     }
 }
